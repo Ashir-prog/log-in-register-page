3c1d3dda9cc56483f092a20f185794c8bc9a12a2
iamgenes <genessambayena@gmail.com>
2020-04-18 23:39:25 +0300

    success connecting to the database

--- myphp.php
+++ myphp.php
@@ -1,20 +1,9 @@
 <?php
-    $servername = "localhost";
-    $username = "admin";
-    $password = "123";
-
-    $conn = new mysql(localhost, admin, 123);
-
-    if ($conn->connect_error){
-        die("Connectin failed: " . $conn->connect_error);
+    $conn = new PDO('mysql:host=localhost;dbname=db_lms', 'root', 'root');
+    if ($conn) {
+        echo "connected successifully to the database";
+    }else{
+        echo "failed to connect to the database";
     }
-    echo "Connected successfully";
 
-        $sql = "CREATE DATABASE myDB";
-        if ($conn->query($sql) ===TRUE){
-            echo "Database created successfully";
-        }else {
-            echo "Error creating database: " .$conn->connect_error;
-        }
-        $conn->close();
-    ?>
+?>
